fdb032ee1c791ec0fdaaf7de1ab079330d0c5dda
Reda Saad <redasaad010260@gmail.com>
2025-11-01 16:22:19 +0200

    commit

--- Terraform/Terraform_AIVmServer/main.tf
+++ Terraform/Terraform_AIVmServer/main.tf
@@ -121,7 +121,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "AI_Server"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = v
   }
 
   os_disk {
--- Terraform/Terraform_AIVmServer/variables.tf
+++ Terraform/Terraform_AIVmServer/variables.tf
@@ -5,3 +5,4 @@ variable "subscription_id" {}
 variable "tenant_id" {}
 variable "client_id" {}
 variable "client_secret" {}
+variable "pip_key" {}
