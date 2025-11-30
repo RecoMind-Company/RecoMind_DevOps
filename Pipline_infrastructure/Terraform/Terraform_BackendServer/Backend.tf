1963b4255bff1f8863c290edba99f71ccac69afe
Reda Saad <Reda.Saad@gmail.com>
2025-11-30 23:08:14 +0200

    commit

---
+++ Pipline_infrastructure/Terraform/Terraform_BackendServer/Backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "DevOpsRG"
+        storage_account_name = "devopsstorage0089"
+        container_name       = "devopsstoragecontainer009"
+        key                  = "Backendterraform.tfstate"
+    }
+}
---
+++ Pipline_infrastructure/Terraform/Terraform_BackendServer/main.tf
@@ -0,0 +1,102 @@
+terraform {
+    required_providers {
+        azurerm = {
+        source  = "hashicorp/azurerm"
+        version = "~> 3.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+    skip_provider_registration = true
+
+    subscription_id = var.subscription_id
+    tenant_id       = var.tenant_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+}
+
+resource "azurerm_resource_group" "rg" {
+    name     = var.name_rg
+    location = var.location_rg 
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet-devops-terraform"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+    name                 = "subnet-terraform"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+    name                = "vm-public-ip"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    allocation_method   = "Static"
+    sku                 = "Standard"
+
+    zones = ["1", "2", "3"]
+}
+
+resource "azurerm_network_interface" "nic" {
+    name                = "vm-nic"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    ip_configuration {
+        name                          = "internal"
+        subnet_id                     = azurerm_subnet.subnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.public_ip.id
+    }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+    name                = "Backend-VM-Server"
+    resource_group_name = azurerm_resource_group.rg.name
+    location            = azurerm_resource_group.rg.location
+    
+    # اختر واحد من هذه الأحجام ⭐
+    size                = "Standard_B2als_v2"       
+    admin_username                  = "BackendAdmin"
+    disable_password_authentication = true
+    
+
+    network_interface_ids = [
+        azurerm_network_interface.nic.id,
+    ]
+
+    admin_ssh_key {
+        username   = "BackendAdmin"
+        public_key = var.ssh_public_key
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "0001-com-ubuntu-server-jammy"
+        sku       = "22_04-lts-gen2"
+        version   = "latest"
+    }
+}
+# Output public IP
+output "public_ip" {
+    value = azurerm_public_ip.public_ip.ip_address
+}
+
+# Output VM Admin Username
+output "vm_admin_username" {
+    value = azurerm_linux_virtual_machine.vm.admin_username
+}
