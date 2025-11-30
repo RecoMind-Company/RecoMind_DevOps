a932bc1e5186308cdc9542d5abe53701a340faa9
Reda Saad <Reda.Saad@gmail.com>
2025-11-30 17:32:22 +0200

    commit

--- Pipline_infrastructure/Terraform/Terraform_BackendVmServer/main.tf
+++ Pipline_infrastructure/Terraform/Terraform_BackendVmServer/main.tf
@@ -1,10 +1,17 @@
 
 provider "azurerm" {
-    features {}
+    features {
+
+        resource_group {
+            prevent_deletion_if_contains_resources = false
+    }
+    }
     subscription_id = var.subscription_id
     tenant_id       = var.tenant_id
     client_id       = var.client_id
     client_secret   = var.client_secret
+
+    
 }
 
 # Resource Group
