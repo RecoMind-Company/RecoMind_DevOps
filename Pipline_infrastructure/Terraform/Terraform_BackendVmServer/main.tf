efcd75180ba45b86fb20a18919e100c203b1a15d
Reda Saad <Reda.Saad@gmail.com>
2025-11-30 17:49:20 +0200

    commit

--- Pipline_infrastructure/Terraform/Terraform_BackendVmServer/main.tf
+++ Pipline_infrastructure/Terraform/Terraform_BackendVmServer/main.tf
@@ -120,7 +120,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     name                = "AI-VM-Server"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
-    size                = "Standard_B2s"
+    size                = "Standard_D2as_v5"
     admin_username      = "BackendServer"
     network_interface_ids = [
         azurerm_network_interface.nic.id,
