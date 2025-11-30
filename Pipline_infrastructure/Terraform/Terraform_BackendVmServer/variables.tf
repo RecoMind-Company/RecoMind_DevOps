86d963133e34e2d6f65895851cea856aced60f70
Reda Saad <Reda.Saad@gmail.com>
2025-11-30 14:30:41 +0200

    commit

---
+++ Pipline_infrastructure/Terraform/Terraform_BackendVmServer/variables.tf
@@ -0,0 +1,12 @@
+variable "name_rg" {
+    default = "DevOpsRG_AI"
+}
+variable "location_rg" {
+    default = "spaincentral"
+}
+
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "ssh_public_key" {}
