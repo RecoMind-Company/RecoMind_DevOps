bc448d47195a70b9b79a14f1845ce59da9f6eb2a
redasaad <redasaad123@gmail.com>
2026-04-19 18:56:05 +0200

    commit

--- Pipeline_infrastructure/Infrastructure_recomind/Terraform/variables.tf
+++ Pipeline_infrastructure/Infrastructure_recomind/Terraform/variables.tf
@@ -9,7 +9,7 @@ variable "subscription_id" {}
 variable "tenant_id" {}
 variable "client_id" {}
 variable "client_secret" {}
-variable "ssh_public_key" {}
+# variable "ssh_public_key" {}
 
 
 variable "resource_group_name" {
